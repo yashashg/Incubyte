7e9393c2ecc19534048df0c057c661f6fb1ac42a
Yashash Gupta <yashashgupta345@gmail.com>
2025-04-21 00:40:24 +0530

    Refactored code for better use and optimization

--- string_calculator.py
+++ string_calculator.py
@@ -1,13 +1,15 @@
 
 def add(number):
-    if number == "":                #Upated the code
-        return 0
-    
-    if "," in number:
-        spliting = number.split(",")
+    sum = 0
+    #Refactored code
 
+    #checking if string is empty
+    if number == "":                
+        return 0
+    else:
+        # Splitting string with comma
+        spliting = number.split(",")
         for n in spliting:
             sum = sum + int(n)
         return(sum)
-    return(int(number))
     
--- test_case.py
+++ test_case.py
@@ -6,3 +6,9 @@ def test_empty_string():
 # Adding a new failed test case
 def test_simple_number():
     assert add("7") == 7
+
+#Adding two numbers, Seperated by comma
+
+def test_multiple_number():
+    assert add("7,3") == 10
+    
